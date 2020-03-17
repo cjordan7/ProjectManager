6e3c281d5ca95ffacd05b31f5b17b2c6893cefb6
cjordan7 <cosmehbvjordan@gmail.com>
2020-03-17 16:28:37 +0100

    Commit on Tue Mar 17 16:28:37 CET 2020

--- pythonTest.py
+++ pythonTest.py
@@ -26,7 +26,7 @@ manage.pushAll(dictio)
 
 print("==================================")
 
-current = Generals.getCurrentDir
+current = Generals.getCurrentDir()
 print(current)
 print("==================================")
 i.getNames("Documents/Programming/Python/pythonManager")
